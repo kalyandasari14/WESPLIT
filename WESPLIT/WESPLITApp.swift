9e920965aa79a02949c4ca124924ab2966a9817a
kalayn <kalyan>
2026-02-12 17:16:34 -0600

    Initial Commit

---
+++ WESPLIT/WESPLITApp.swift
@@ -0,0 +1,17 @@
+//
+//  WESPLITApp.swift
+//  WESPLIT
+//
+//  Created by kalyan on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WESPLITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
